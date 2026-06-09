eb11c1ed637b6bf0a2fc97691046e58b0ed40156
huynhgia2212 <huynhgia714@gmail.com>
2026-06-09 17:44:07 +0700

    Fix port binding issue cho Docker tren Render

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+ // Bật CORS để lát nữa Frontend có thể gọi API mà không bị chặn
+  app.enableCors(); 
+
+  // Thay vì fix cứng 3000, ta dùng process.env.PORT của Cloud.
+  // Tham số '0.0.0.0' cực kỳ quan trọng để Docker cho phép truy cập từ bên ngoài.
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
